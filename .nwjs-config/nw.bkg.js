4582881f08222aecb8068d861fd386b6f397fb2b
Patrick A Lima <patrickalima@nuinalp.com>
2019-02-07 21:51:30 -0200

    Added initial files from new Electron version

--- .nwjs-config/nw.bkg.js
+++
@@ -1,9 +0,0 @@
-const isProduction = process.env.NODE_ENV === 'production'
-const config = require('./config');
-
-const url = isProduction ? './dist/app/index.html' : `http://${config.dev.host}:${config.dev.port}`;
-const winConfig = nw.App.manifest.window;
-
-winConfig.new_instance = true;
-
-nw.Window.open(url, winConfig);
